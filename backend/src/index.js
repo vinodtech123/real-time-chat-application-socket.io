a09e3d279fab6825345902fb2d29fbae96b433b7
vinodtech123 <vinodabnani321@gmail.com>
2025-06-15 13:13:33 +0530

    user authenticate api completed

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
